37bceacc2e1092e49488fa8277b3ba836fe1599b
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-06-08 17:32:29 -0400

    9.3.8

--- app.js
+++ app.js
@@ -127,15 +127,20 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
             message: 'Provide some information about yourself:',
-            validate: informationProv => {
-                if(informationProv) {
+            when: ({confirmAbout}) => {
+                if(confirmAbout) {
                     return true;
                 }
                 else{
-                    console.log('Please enter some information!');
                     return false;
                 }
             }
